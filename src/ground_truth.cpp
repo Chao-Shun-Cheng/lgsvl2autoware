06a952354ef193c2a67ed6871466404ed9691bf1
Chao-Shun-Cheng <chao.shun.cheng.tw@gmail.com>
2022-02-12 16:49:56 +0800

    add polygon of objects

--- src/ground_truth.cpp
+++ src/ground_truth.cpp
@@ -7,7 +7,7 @@
 using namespace std;
 
 autoware_msgs::DetectedObjectArray result;
-
+void generatePolygon(autoware_msgs::DetectedObject &object);
 void d3_detections_callback(const lgsvl_msgs::Detection3DArray &msg)
 {
     ros::Time now = ros::Time::now();
@@ -18,19 +18,22 @@ void d3_detections_callback(const lgsvl_msgs::Detection3DArray &msg)
     for (int i = 0; i < msg.detections.size(); i++) {
         autoware_msgs::DetectedObject object;
         object.header = data.header;
+        object.convex_hull.header = data.header;
+        object.space_frame = "velodyne";
+        object.pose_reliable = true;
+        object.velocity_reliable = true;
+        object.valid = true;
+        object.source = "lgsvl";
+        object.latency = latency;
+
         object.id = msg.detections[i].id;
         object.label = msg.detections[i].label;
         object.score = msg.detections[i].score;
         object.velocity = msg.detections[i].velocity;
         object.pose = msg.detections[i].bbox.position;
-        object.source = "lgsvl";
-        object.latency = latency;
         object.dimensions = msg.detections[i].bbox.size;
-        object.pose_reliable = true;
-        object.velocity_reliable = true;
-        object.valid = true;
-        object.space_frame = "velodyne";
-        object.convex_hull.header = data.header;
+        generatePolygon(object);
+        
         data.objects.push_back(object);
     }
     result = data;
@@ -50,4 +53,61 @@ int main(int argc, char **argv)
         loop_rate.sleep();
     }
     return 0;
+}
+
+void generatePolygon(autoware_msgs::DetectedObject &object)
+{
+    float longitudinal_distance = object.dimensions.y / 2;
+    float lateral_distance = object.dimensions.x / 2;
+    float vertial_distance = object.dimensions.z;
+
+    geometry_msgs::Point32 p;
+    
+    // FRU (front, right, up)
+    p.y = object.pose.position.y + longitudinal_distance;
+    p.x = object.pose.position.x + lateral_distance;
+    p.z = vertial_distance;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // FRD
+    p.y = object.pose.position.y + longitudinal_distance;
+    p.x = object.pose.position.x + lateral_distance;
+    p.z = 0;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // FLU
+    p.y = object.pose.position.y + longitudinal_distance;
+    p.x = object.pose.position.x - lateral_distance;
+    p.z = vertial_distance;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // FLD
+    p.y = object.pose.position.y + longitudinal_distance;
+    p.x = object.pose.position.x - lateral_distance;
+    p.z = 0;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // BRU (back, right, up)
+    p.y = object.pose.position.y - longitudinal_distance;
+    p.x = object.pose.position.x + lateral_distance;
+    p.z = vertial_distance;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // BRD
+    p.y = object.pose.position.y - longitudinal_distance;
+    p.x = object.pose.position.x + lateral_distance;
+    p.z = 0;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // BLU
+    p.y = object.pose.position.y - longitudinal_distance;
+    p.x = object.pose.position.x - lateral_distance;
+    p.z = vertial_distance;
+    object.convex_hull.polygon.points.push_back(p);
+
+    // BLD
+    p.y = object.pose.position.y - longitudinal_distance;
+    p.x = object.pose.position.x - lateral_distance;
+    p.z = 0;
+    object.convex_hull.polygon.points.push_back(p);
 }
